d709c4178d86ad8436a49620cd1a1c2cf7c48381
xrowkristina <kristina@xrow.de>
2015-02-13 16:13:02 +0100

    bugfixing

--- autoloads/xrowmetadataoperator.php
+++ autoloads/xrowmetadataoperator.php
@@ -43,72 +43,77 @@ class xrowMetaDataOperator
             {
                 if( isset( $namedParameters['node_id'] ) )
                 {
-                    $cur_node = eZContentObjectTreeNode::fetch( $namedParameters['node_id'] );
-                    if( $cur_node instanceof eZContentObjectTreeNode )
+                    $node = eZContentObjectTreeNode::fetch( $namedParameters['node_id'] );
+                    if( $node instanceof eZContentObjectTreeNode )
                     {
-                        $cur_parent=$cur_node->fetchParent();
-                        $obj_name=$cur_node->getName();
-                        $obj_count =$cur_node->subTreeCount(array( 'IgnoreVisibility' => true ));
-                        $path_array_temp=array_reverse(explode('/',$cur_node->pathWithNames()));
-                        $obj_path =implode(' | ',$path_array_temp);
-                        $obj_parentname =$cur_parent->Name;
-                        
-                        $operatorValue = xrowMetaDataFunctions::fetchByObject( $cur_node->attribute( 'object' ) );
-                        
-                        $search_title=$operatorValue->title;
-                        $search_description = $operatorValue->description;
+                        $cur_parent = $node->fetchParent();
+                        $obj_name = $node->getName();
+                        $obj_count = $node->subTreeCount(array( 'IgnoreVisibility' => true ));
+                        $obj_path = str_replace('/',' | ',(string)$node->pathWithNames());
+                        $obj_parentname = $cur_parent->Name;
 
-                        $placeholder_title_array=self::getPlaceholders($search_title);
-                        $placeholder_title_description=self::getPlaceholders($search_description);
-                         
-                        if(count($placeholder_title_array) !== 0)
+                        $operatorValue = xrowMetaDataFunctions::fetchByObject( $node->attribute( 'object' ) );
+                        if($operatorValue !== false)
                         {
-                            foreach($placeholder_title_array as $placeholder_title)
+                            if(isset($operatorValue->title))
                             {
-                                switch($placeholder_title)
+                                $search_title = $operatorValue->title;
+                                $placeholder_title_array = self::getPlaceholders($search_title);
+                                if(count($placeholder_title_array) !== 0)
                                 {
-                                    case "count":
-                                        $meta_title=str_replace("[count]",$obj_count,$search_title);
-                                        continue;
-                                    case "name":
-                                        $meta_title=str_replace("[name]",$obj_name,$search_title);
-                                        continue;
-                                    case "path":
-                                        $meta_title=str_replace("[path]",$obj_path,$search_title);
-                                        continue;
-                                    case "parentname":
-                                        $meta_title=str_replace("[parentname]",$obj_parentname,$search_title);
-                                        continue;
+                                    foreach($placeholder_title_array as $placeholder_title)
+                                    {
+                                        switch($placeholder_title)
+                                        {
+                                            case "count":
+                                                $meta_title = str_replace("[count]", $obj_count, $search_title);
+                                                continue;
+                                            case "name":
+                                                $meta_title = str_replace("[name]", $obj_name, $search_title);
+                                                continue;
+                                            case "path":
+                                                $meta_title = str_replace("[path]", $obj_path, $search_title);
+                                                continue;
+                                            case "parentname":
+                                                $meta_title = str_replace("[parentname]", $obj_parentname, $search_title);
+                                                continue;
+                                        }
+                                        $search_title = $meta_title;
+                                    }
+                                    $operatorValue->title = $search_title;
+                                    unset($search_title);
                                 }
-                                $search_title = $meta_title;
+                            }
+                            if(isset($operatorValue->description))
+                            {
+                                $search_description = $operatorValue->description;
+                                $placeholder_title_description = self::getPlaceholders($search_description);
+                                if(count($placeholder_title_description) !== 0)
+                                {
+                                    foreach($placeholder_title_description as $placeholder_description)
+                                    {
+                                        switch($placeholder_description)
+                                        {
+                                            case "count":
+                                                $meta_description = str_replace("[count]", $obj_count, $search_description);
+                                                continue;
+                                            case "name":
+                                                $meta_description = str_replace("[name]", $obj_name, $search_description);
+                                                continue;
+                                            case "path":
+                                                $meta_description = str_replace("[path]", $obj_path, $search_description);
+                                                continue;
+                                            case "parentname":
+                                                $meta_description = str_replace("[parentname]", $obj_parentname, $search_description);
+                                                continue;
+                                        }
+                                        $search_description = $meta_description;
+                                    }
+                                    $operatorValue->description = $search_description;
+                                    unset($search_description);
+                                }
                             }
                         }
-                         
-                        if(count($placeholder_title_description) !== 0)
-                        {
-                            foreach($placeholder_title_description as $placeholder_description)
-                            {
-                                switch($placeholder_description)
-                                {
-                                    case "count":
-                                        $meta_description=str_replace("[count]",$obj_count,$search_description);
-                                        continue;
-                                    case "name":
-                                        $meta_description=str_replace("[name]",$obj_name,$search_description);
-                                        continue;
-                                    case "path":
-                                        $meta_description=str_replace("[path]",$obj_path,$search_description);
-                                        continue;
-                                    case "parentname":
-                                        $meta_description=str_replace("[parentname]",$obj_parentname,$search_description);
-                                        continue;
-                                }
-                                $search_description = $meta_description;
-                            }
-                        }
-                        
-                        $operatorValue->title=$search_title;
-                        $operatorValue->description=$search_description;
                     }
                     else
                     {
@@ -121,7 +126,6 @@ class xrowMetaDataOperator
                 }
             }break;
         }
-        unset($search_title,$search_description);
     }
 }
 
